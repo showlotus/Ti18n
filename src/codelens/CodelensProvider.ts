7fe9b7beb88e33b46facab7243ee2446c9865e03
showlotus <yaoxuu@qq.com>
2023-08-29 18:45:03 +0800

    *

---
+++ src/codelens/CodelensProvider.ts
@@ -0,0 +1,40 @@
+import * as vscode from "vscode";
+import { checkIsTargetDocument, getTokenRanges } from "../utils";
+
+/**
+ * CodelensProvider
+ */
+export class CodelensProvider implements vscode.CodeLensProvider {
+  private onDidChangeCodeLensesEmitter: vscode.EventEmitter<void> = new vscode.EventEmitter<void>();
+
+  get onDidChangeCodeLenses(): vscode.Event<void> {
+    return this.onDidChangeCodeLensesEmitter.event;
+  }
+
+  public refresh(): void {
+    this.onDidChangeCodeLensesEmitter.fire();
+  }
+
+  public provideCodeLenses(
+    document: vscode.TextDocument,
+    token: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.CodeLens[]> {
+    if (!checkIsTargetDocument(document)) {
+      return;
+    }
+
+    const codeLens: vscode.CodeLens[] = [];
+    getTokenRanges(document, (key: string, range: vscode.Range) => {
+      codeLens.push(
+        new vscode.CodeLens(range, {
+          title: key,
+          tooltip: key + key,
+          command: "test.ttttt",
+          arguments: [],
+        })
+      );
+    });
+
+    return codeLens;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,16 @@ import * as vscode from "vscode";
 import { appendStyle } from "./utils/ui";
 import { getConfiguration, loadConfigJSON } from "./utils/workspace";
 import { openDocumentRevealTokenRange } from "./utils";
+import { CodelensProvider } from "./codelens/CodelensProvider";
 
 export function activate(context: vscode.ExtensionContext) {
   const enable = getConfiguration("enable");
   if (!enable) {
     return;
   }
+
+  const codelensProvider = new CodelensProvider();
+  vscode.languages.registerCodeLensProvider("*", codelensProvider);
 
   const { commands, window, workspace } = vscode;
   const updateStyle = (editor?: vscode.TextEditor) => {
