15024a45663d67704cc7c1c055aa73af0c3123ad
showlotus <2257030399@qq.com>
2024-04-24 23:21:43 +0800

    *

--- src/modules/Parser.ts
+++ src/modules/Parser.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode'
 import * as path from 'path'
 import * as fg from 'fast-glob'
-import jsonParser from 'json-to-ast'
 import * as t from '@babel/types'
 import * as babelParser from '@babel/parser'
 import {
